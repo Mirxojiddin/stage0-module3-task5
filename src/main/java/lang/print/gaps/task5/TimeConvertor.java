4d49eb54e09712ba41512a047b4bf9aa44a2442b
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-01 22:11:43 +0500

    javob 1

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds;
+
+        seconds=minutes*60;
+
+        System.out.println(seconds);
     }
 }
